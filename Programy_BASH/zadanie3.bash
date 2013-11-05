76d42af69f2060682adfb90b35f642e6ecf5523a
Łukasz Petke <lukpetke@gmail.com>
2013-11-05 19:49:50 +0100

    Update zadanie3.bash

--- Programy_BASH/zadanie3.bash
+++ Programy_BASH/zadanie3.bash
@@ -1,15 +1,17 @@
-#skrypt dla zadania nr.2
-#program sprawdza czy w katalogu w którym się znajdujemy istnieje plik skrypt102.bash jeżeli nie, to tworzy go i wpisuje w pierwszej lini wpisuje napis "To jest nowy plik"
+#skrypt dla zadania nr.3
+#program kopiuje wszystkie pliki txt do podanego jako argument katalogu.. jeżeli takiego katalogu niema to zostaje on stworzony
 
-kopiuje=`echo 'To jest nowy plik' >>  skrypt102.bach`
-if [ -e skrypt102.bash ]
+tworze=`mkdir $1`
+kopiuje=`cp *.txt $1`
+
+if [ -e $1 ]  # -e sprawdza czy podana wartość(katalog) istnieje
 then 
-    echo "skopiowałem wszystkie pliki o rozszerzeniu .txt z katalogu $PWD  do katalogu $1"
+    echo "skopiowałem wszystkie pliki o rozszerzeniu .txt z katalogu $PWD  do katalogu $1" #jeżeli istnieje to kopiuje wszystkie pliki o rozszerzeniu .txt
 else 
-    echo "Niestety podany katalog nie istnieje, ale stworzę go za Ciebie $kopiuje"
+    echo "Niestety podany katalog nie istnieje, ale stworzę go za Ciebie $tworze i skopiuję wszystkie pliki o rozszerzeniu .txt $kopiuje" #jeżeli nie to tworzy katalog i wtedy kopiuje wszystko
     echo "Trwa kopiowanie..."
-sleep 5
-    echo "OK. Katalog $1 został utworzony i przekopiowałem do niego wszystkie pliki
+sleep 5     #zbędny dodatek zatrzymujący program na 5 sekund
+    echo "OK. Katalog $1 został utworzony i przekopiowałem do niego wszystkie pliki"
 
 fi
 
