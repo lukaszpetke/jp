e19020e4d2473a2aac4f84ca6b7a3eab549124c7
Łukasz Petke <lukpetke@gmail.com>
2013-11-09 07:14:03 +0100

    Update zadanie3.bash

--- Programy_BASH/zadanie3.bash
+++ Programy_BASH/zadanie3.bash
@@ -1,17 +1,9 @@
-#skrypt dla zadania nr.3
-#program kopiuje wszystkie pliki txt do podanego jako argument katalogu.. jeżeli takiego katalogu niema to zostaje on stworzony
+#skrypt dla zadania nr.1
+#program sprawdza czy w katalogu w którym się znajdujemy istnieje plik zadanie2.bash
 
-if [ -e $1 ]  # -e sprawdza czy podana wartość(katalog) istnieje
-then 
-    echo "skopiowałem wszystkie pliki o rozszerzeniu .txt z katalogu $PWD  do katalogu $1" #jeżeli istnieje to kopiuje wszystkie pliki o rozszerzeniu .txt
-else 
-    echo "Niestety podany katalog nie istnieje, ale stworzę go za Ciebie i skopiuję wszystkie pliki o rozszerzeniu .txt" #jeżeli nie to tworzy katalog i wtedy kopiuje wszystko
-mkdir $1
-cp *.txt $1 
-    echo "Trwa kopiowanie..."
-sleep 5     #zbędny dodatek zatrzymujący program na 5 sekund
-    echo "OK. Katalog $1 został utworzony i przekopiowałem do niego wszystkie pliki"
-
+if [ -e zadanie2.bash ]  # -e służy do sprawdzenia czy podany plik istnieje 
+then echo "Masz plik zadanie2.bash"
+else echo "Nie masz pliku zadanie2.bash"
 fi
 
 exit 0
