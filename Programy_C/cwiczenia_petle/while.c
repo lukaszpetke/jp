00ab8ff77008b67113ea897340ec73ff1bca2c44
Łukasz Petke <lukpetke@gmail.com>
2013-11-09 07:18:22 +0100

    Create while.c

---
+++ Programy_C/cwiczenia_petle/while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  int warun; 
+//pierwsza jest zmienna uzywana w petli
+//druga rzecz to warunek ktory jest sprawdzane PRZED kazdym wykonaniem petli
+//trzeci element, to kod ktory zostanie zrealizowany PO kazdym wykonaniu petli
+for(warun=1; warun < 11; warun++){
+printf("%i zielonych butelek stoi pod sciana\n, warun");
+}
+  return 0;
+}
