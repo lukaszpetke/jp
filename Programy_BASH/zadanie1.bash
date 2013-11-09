a2bd435fb23a98d74556d43513ff50ca20d9ce0d
Łukasz Petke <lukpetke@gmail.com>
2013-11-09 07:13:12 +0100

    Update zadanie1.bash

--- Programy_BASH/zadanie1.bash
+++ Programy_BASH/zadanie1.bash
@@ -1,9 +1,9 @@
 #skrypt dla zadania nr.1
-#program sprawdza czy w katalogu w którym się znajdujemy istnieje plik skrypt2.bash
+#program sprawdza czy w katalogu w którym się znajdujemy istnieje plik zadanie2.bash
 
-if [ -e skrypt2.bash ]
-then echo "Masz plik skrypt2.bash"
-else echo "Nie masz pliku skrypt2.bash"
+if [ -e zadanie2.bash ]  # -e służy do sprawdzenia czy podany plik istnieje 
+then echo "Masz plik zadanie2.bash"
+else echo "Nie masz pliku zadanie2.bash"
 fi
 
 exit 0
