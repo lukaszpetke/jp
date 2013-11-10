ca94c49a0c0d1ce8b32a7757cde5cc910db25e45
Łukasz Petke <lukpetke@gmail.com>
2013-11-10 12:41:57 +0100

    Create 1wer2_zad6.c

---
+++ Programy_C/cwiczenia_petle/1wer2_zad6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  int i, sum=0;
+  for (i = 1; i<=20 ; i++)
+    {
+      sum=sum+i;
+    }
+      printf("Suma liczb naturalnych od 1 do 20 to %d\n", sum);
+ return 0;
+}
