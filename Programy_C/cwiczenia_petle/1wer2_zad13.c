5cbfe898b7621715675f88bfaf756cd35d4724ac
Łukasz Petke <lukpetke@gmail.com>
2013-11-10 12:44:55 +0100

    Create 1wer2_zad13.c

---
+++ Programy_C/cwiczenia_petle/1wer2_zad13.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+  int a, i, N;
+
+  printf("podaj liczbe calkowita:");
+  scanf("%d", &N);
+  a=0;
+  for (i=0; a<N; i++){    
+    a=a+i;
+  }
+  i=i-2;
+  printf("największą liczbą naturalną n jest: %d \n", i);
+
+  return 0;
+}
