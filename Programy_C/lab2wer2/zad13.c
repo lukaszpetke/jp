80f1a6c7a771370da772df5a1750741631f56284
Łukasz Petke <lukpetke@gmail.com>
2013-11-18 22:28:26 +0100

    Create zad13.c

---
+++ Programy_C/lab2wer2/zad13.c
@@ -0,0 +1,29 @@
+//wczyta od użytkownika dane do pięcioelementowej tablicy liczb całkowitych, a następnie wypisze jej najmniejszy element na ekran;
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int tab1[5],i,naj;
+
+  for(i=0; i<5; i++){
+    printf("podaj tab1[%d]",i);
+    scanf("%d",&tab1[i]);
+    }
+naj=tab1[i];
+  for(i=0; i<5; i++) {
+
+		printf("tab1[%d]=%d\n",i,tab1[i]);
+			 if (naj>tab1[i])
+				{
+				  naj=tab1[i];
+				}
+				
+	}
+
+ printf("najmniejszy podany element to= %d\n",naj);
+
+
+    return 0;
+}
