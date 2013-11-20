fec8e5fa7ddbe9baf5c050b4f293fa271f467d12
Łukasz Petke <lukpetke@gmail.com>
2013-11-20 20:06:31 +0100

    Create zadanie13.bash

---
+++ Programy_BASH/zadanie13.bash
@@ -0,0 +1,4 @@
+#skrypt dla zadania nr.13
+#zapisuje do pliku o nazwie "plik.usr" pełną informację o użytkownikach którzy mają odblokowane przyjmowanie komunikatów na termional. 
+
+who -w >> plik.usr
