b3881d2d2d34a90e5fbc1a97465f1f788c370256
Łukasz Petke <lukpetke@gmail.com>
2013-11-24 09:24:16 +0100

    Create elementarz1.c

---
+++ Programy_C/lab2wer2/elementarz1.c
@@ -0,0 +1,18 @@
+//Napisz program obliczający wartość każdego z poniższych wyrażeń:
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int n1, n2, n3;
+  
+  n1=5+3*8/2-3;
+  n2=2%2+2*2-2/2;
+  n3=2*4*(5+9.0/2);
+
+  printf("n1=%d ; n2=%d ; n3=%d ;\n",n1,n2,n3);
+
+
+    return 0;
+}
