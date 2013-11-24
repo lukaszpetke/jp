3d276a846111da577f30d90584d9f3bda1bf5460
Łukasz Petke <lukpetke@gmail.com>
2013-11-24 10:33:13 +0100

    Create zad1.c

---
+++ Programy_C/elementarz2/zad1.c
@@ -0,0 +1,20 @@
+//program dla podanej z klawiatury liczby M ma obliczyć najmniejszą liczbę n, taką że
+//1 + 2 + 3 +...+ n >= M
+
+#include <stdio.h>
+int main() {
+  int M, n = 0;
+  int suma = 0;
+  int i = 1;
+
+  printf("Podaj liczbe M: ");
+  scanf("%d", &M);
+
+  while ( suma < M ) {
+     suma += i;
+     i += 1;
+     n += 1;
+  }
+    printf("Szukane n to: %d\n", n);
+  return 0;
+}
