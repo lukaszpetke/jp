b87368091a6f677fb890a504dd0f8abeb0fb5eff
Łukasz Petke <lukpetke@gmail.com>
2013-12-14 23:42:15 +0100

    Update zad1.c

--- Programy_C/elementarz3/zad1.c
+++ Programy_C/elementarz3/zad1.c
@@ -1,4 +1,4 @@
-1. W poniższym programie, który kopiując wejście na wyjście zastępuje kazdy znak tabulacji przez \t oraz każdy znak \ przez dwa takie znaki, zostały przestawione wiersze. Uporzadkuj je, tak aby powstał działający program.
+//1. W poniższym programie, który kopiując wejście na wyjście zastępuje kazdy znak tabulacji przez \t oraz każdy znak \ przez dwa takie znaki, zostały przestawione wiersze. Uporzadkuj je, tak aby powstał działający program.
 
 #include <stdio.h>
 
