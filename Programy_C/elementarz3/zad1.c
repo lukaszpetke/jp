8821d92e7b7d3a24ad565744b0dd26c85fb34ed7
Łukasz Petke <lukpetke@gmail.com>
2013-12-14 23:37:00 +0100

    Update zad1.c

--- Programy_C/elementarz3/zad1.c
+++ Programy_C/elementarz3/zad1.c
@@ -1,1 +1,21 @@
-d
+1. W poniższym programie, który kopiując wejście na wyjście zastępuje kazdy znak tabulacji przez \t oraz każdy znak \ przez dwa takie znaki, zostały przestawione wiersze. Uporzadkuj je, tak aby powstał działający program.
+
+#include <stdio.h>
+
+int main() {
+int c;
+
+  while ((c=getchar())!=EOF)
+
+  if (c=='\t')
+    printf("\\t");
+
+  else if (c=='\\')
+    printf("\\\\");
+
+  else
+    putchar(c);
+
+return 0;
+
+}
