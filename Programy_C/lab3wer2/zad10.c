5730c1f03c84ffb5f2d57993a9890c086ba03922
Łukasz Petke <lukpetke@gmail.com>
2013-12-15 10:43:47 +0100

    Create zad10.c

---
+++ Programy_C/lab3wer2/zad10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h> 
+
+int main()
+{
+  int s, a, b;
+  a=1;
+  s=0;
+
+  printf("Podaj liczbe: ");
+  scanf("%d", &b);
+
+  for (a; a<=b; a++){
+    s=s+a;
+ 
+    //printf("silnia=%d | a=%d\n",s, a);
+  }
+  printf("silnia liczby =%d to=%d\n",b ,s);
+
+    return 0;
+}
