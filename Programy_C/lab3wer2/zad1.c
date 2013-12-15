ee8c373be085ed0f71f64d2744b0204826287809
Łukasz Petke <lukpetke@gmail.com>
2013-12-15 09:45:47 +0100

    Create zad1.c

---
+++ Programy_C/lab3wer2/zad1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h> 
+
+int main()
+{
+    int a, b, p;
+
+    printf("Podaj liczbe a: ");
+    scanf("%d", &a);
+    printf("Podaj liczbe b: ");
+    scanf("%d", &b);
+    p=a*b;
+    printf("Pole prostokata jest rowne=%d\n",p);
+
+    return 0;
+}
