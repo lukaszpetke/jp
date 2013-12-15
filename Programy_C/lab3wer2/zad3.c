33f930f75e2274d44ed4353b3a6cbb3c00174f43
Łukasz Petke <lukpetke@gmail.com>
2013-12-15 12:58:47 +0100

    Update zad3.c

--- Programy_C/lab3wer2/zad3.c
+++ Programy_C/lab3wer2/zad3.c
@@ -20,3 +20,38 @@ int main()
 }
 
 
+
+
+// trzeba dokonczyc
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h> 
+
+int objs(){
+  int obj;
+  obj=(h*p)/3;
+  return obj;
+}
+
+
+int main()
+{
+  int o, h, p, r, pi;
+  pi=3.14;
+    printf("Podaj promien podstawy stozka r: ");
+    scanf("%d", &r);
+    p=pi*(r*r);
+
+    printf("Podaj wysokosc stozka: ");
+    scanf("%d", &h);
+
+    o=objs(
+    printf("Objetosc stozka wynosi:%d\n",o);
+
+    return 0;
+}
+
+
+
+
