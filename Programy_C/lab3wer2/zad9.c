2ba61689a482314fd12adccda9ae1e2c0563d7eb
Łukasz Petke <lukpetke@gmail.com>
2013-12-15 10:26:19 +0100

    Create zad9.c

---
+++ Programy_C/lab3wer2/zad9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h> 
+
+int main()
+{
+  int suma, a;
+  suma=0;
+  a=11;
+  while (a<=50){
+    suma=suma+a;
+    a++;
+ 
+    //  printf("suma=%d | a=%d ",suma,a);
+  }
+  printf("suma liczb od 11 do 50=%d\n",suma);
+
+
+    return 0;
+}
+
+
+
+
